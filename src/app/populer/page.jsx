89240db997f6aa36157c403be336cf019e9461c5
Jason <jasonsaputra08@gmail.com>
2024-12-04 20:35:36 +0700

    Add Paginatio Simple Logic

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -1,7 +1,32 @@
-const Page = () => {
-    return (
-        <div>Popular Page</div>
-    )
-}
+"use client";
 
-export default Page
+import React, { useEffect, useState } from "react";
+import HeaderMenu from "@/components/Utilities/HeaderMenu";
+import AnimeList from "@/components/AnimeList";
+import Pagination from "@/components/Utilities/Pagination";
+
+const Page = () => {
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(`
+            ${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`);
+    const data = await response.json();
+    setTopAnime(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  return (
+    <>
+      <HeaderMenu title={`Anime Terpopuler #${page}`}/>
+      <AnimeList api={topAnime} />
+      <Pagination page={page} lastPage={topAnime.pagination?.last_visible_page} setPage={setPage} />
+    </>
+  );
+};
+
+export default Page;
