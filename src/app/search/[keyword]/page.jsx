07d70703f29b2b633cdd821a425dfa68d5d11db4
Jason <jasonsaputra08@gmail.com>
2024-12-05 13:23:10 +0700

    Reusable API Call

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,3 +1,4 @@
+import { getAnimeResponse } from "@/app/services/api-services";
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
 import { notFound } from "next/navigation";
@@ -6,9 +7,7 @@ const Page = async ({ params }) => {
   const { keyword } = await params
   const decodedKeyword = decodeURI(keyword)
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`);
-  const searchAnime = await response.json();
-
+  const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
   if (searchAnime.data.length === 0) {
     notFound();
   }
