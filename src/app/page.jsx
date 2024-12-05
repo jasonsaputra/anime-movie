8af051f953b947daac6251d39c225516c4f31ace
Jason <jasonsaputra08@gmail.com>
2024-12-05 15:18:41 +0700

    Add Recommendation Anime List

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,23 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse } from "@/services/api-services";
+import { getAnimeResponse, getNestedAnimeResponse } from "@/services/api-services";
 
 const Page = async () => {
   // const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
   // const topAnime = await response.json();
 
   const topAnime = await getAnimeResponse("top/anime", "limit=8");
-
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
+  recommendedAnime = {data: recommendedAnime.slice(0, 8) }
   return (
     <>
       <section>
-        <Header
-          title="Paling Populer"
-          linkTitle="Lihat Semua"
-          linkHref="/populer"
-        />
+        <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/populer" />
         <AnimeList api={topAnime} />
+      </section>
+      <section>
+        <Header title="Rekomendasi" />
+        <AnimeList api={recommendedAnime} />
       </section>
     </>
   );
