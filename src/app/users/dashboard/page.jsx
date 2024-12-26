eaaaf3f3efb8b5825b6213ee10be964f71f8c1f6
Jason <jasonsaputra08@gmail.com>
2024-12-26 15:55:28 +0700

    Validasi User Route & UI Dashboard Collection

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,17 +1,30 @@
 import { authUserSession } from "@/services/auth-services.js";
 import Image from "next/image";
+import Link from "next/link"
 
 const Page = async () => {
+  const user = await authUserSession();
 
-    const user = await authUserSession()
-    console.log(user)
-    return (
-        <div className="text-color-primary">
-            <h3>DASHBOARD</h3>
-            <h5>Welcome, {user?.name}</h5>
-            <Image src={user?.image} alt="" width={250} height={250} />
-        </div>
-    );
+  return (
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-2xl font-bold">Welcome, {user?.name}</h5>
+      <Image src={user?.image} alt="user-image" width={250} height={250} />
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href="/users/dashboard/collection"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Collection
+        </Link>
+        <Link
+          href="/users/dashboard/comment"
+          className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl"
+        >
+          My Comment
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
