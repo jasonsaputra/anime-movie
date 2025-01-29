107bc52daf1825b0f0dc56192330ee9c61e04cb6
Jason <jasonsaputra08@gmail.com>
2025-01-29 21:55:49 +0700

    change aired date to year only

--- src/components/AnimeList/SidebarAnimeList.jsx
+++ src/components/AnimeList/SidebarAnimeList.jsx
@@ -6,6 +6,8 @@ const SidebarAnimeList = ({ api }) => {
   return (
     <div className="h-max w-full shrink-0 flex flex-col gap-3 pl-4">
       {api.data?.map((anime, index) => {
+        console.log(api.data)
+        const airedYear = anime.aired.from ? new Date(anime.aired.from).getFullYear() : '';
         return(
           <Link key={index} href={`/anime/${anime.mal_id}`} className="flex cursor-pointer text-color-primary hover:text-color-accent transition-all">
             <Image
@@ -15,9 +17,9 @@ const SidebarAnimeList = ({ api }) => {
               height={100}
               className="object-cover rounded-lg aspect-[3/4]"
             />
-            <section className="flex flex-col px-5 w-full">
+            <section className="flex flex-col px-2 w-full">
                 <div className="flex justify-between items-center p-2 md:text-xs text-xs text-color-disabled">
-                    {anime.aired.from}
+                    {airedYear}
                 </div>
                 <p className="font-medium text-md px-2 line-clamp-1">{anime.title}</p>
             </section>
